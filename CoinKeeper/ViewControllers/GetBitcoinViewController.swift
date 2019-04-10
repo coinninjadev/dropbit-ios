3703f298526a8bb9a4129438f5b7c7ec87fd1878
BJ Miller <bj.miller@coinninja.com>
2019-04-10 18:18:34 -0400

    Add dollar symbol image and GetBitcoinVC

---
+++ CoinKeeper/ViewControllers/GetBitcoinViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GetBitcoinViewController.swift
+//  DropBit
+//
+//  Created by BJ Miller on 4/10/19.
+//  Copyright © 2019 Coin Ninja, LLC. All rights reserved.
+//
+
+import UIKit
+
+class GetBitcoinViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
