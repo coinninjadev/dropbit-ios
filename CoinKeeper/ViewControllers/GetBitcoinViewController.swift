1deffed07c0c20d97a1c13664feba940f8506776
BJ Miller <bj.miller@coinninja.com>
2019-04-11 11:45:10 -0400

    Reset storyboard, add buttons and title

--- CoinKeeper/ViewControllers/GetBitcoinViewController.swift
+++ CoinKeeper/ViewControllers/GetBitcoinViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 final class GetBitcoinViewController: BaseViewController, StoryboardInitializable {
 
+  @IBOutlet var headerLabel: UILabel!
+  @IBOutlet var findATMButton: PrimaryActionButton!
+  @IBOutlet var buyWithCreditCardButton: PrimaryActionButton!
+  @IBOutlet var buyWithGiftCardButton: PrimaryActionButton!
+
   weak var urlOpener: URLOpener?
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    headerLabel.textColor = Theme.Color.grayText.color
+    headerLabel.font = Theme.Font.sendingBitcoinAmount.font
+
+
+  }
+
+//  private func attributedSymbol(for image: UIImage) -> NSAttributedString {
+//    let textAttribute = NSTextAttachment()
+//    textAttribute.image = image
+//    let size = CGFloat(20)
+//    textAttribute.bounds = CGRect(x: -0, y: (-size / (size / 4)),
+//                                  width: size, height: size)
+//    return NSAttributedString(attachment: textAttribute)
+//  }
+
+  @IBAction func findATM() {
+
+  }
+
+  @IBAction func buyWithCreditCard() {
+
+  }
+
+  @IBAction func buyWithGiftCard() {
+
   }
 }
