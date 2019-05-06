867f1db1e6b13bf40855304f01a12c11cf9ebb1a
BJ Miller <bj.miller@coinninja.com>
2019-05-06 08:49:27 -0400

    Add TwitterRequestable protocol

---
+++ CoinKeeper/Networking/NetworkManager/NetworkManager+Twitter.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager+Twitter.swift
+//  DropBit
+//
+//  Created by BJ Miller on 5/6/19.
+//  Copyright © 2019 Coin Ninja, LLC. All rights reserved.
+//
+
+import PromiseKit
+import OAuthSwift
+
+protocol TwitterRequestable: AnyObject {
+}
