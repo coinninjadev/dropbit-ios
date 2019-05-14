af4928c32b460f2c98b0eed76129fcf31bfe65f5
BJ Miller <bj.miller@coinninja.com>
2019-05-14 11:37:35 -0400

    Add friends endpoint

--- CoinKeeper/Networking/Twitter/TwitterEndpoints.swift
+++ CoinKeeper/Networking/Twitter/TwitterEndpoints.swift
@@ -11,11 +11,13 @@ import Foundation
 enum TwitterEndpoints {
   case getUser
   case search
+  case friends
 
   var urlString: String {
     switch self {
     case .getUser: return "https://api.twitter.com/1.1/users/show.json"
     case .search: return "https://api.twitter.com/1.1/users/search.json"
+    case .friends: return "https://api.twitter.com/1.1/friends/list.json"
     }
   }
 }
