c8efe0c39f514d104a40ed6eb756dfa75a530411
Ben Winters <ben.winters@coinninja.com>
2019-05-15 14:13:44 -0400

    Move remote notification functions to separate file

---
+++ CoinKeeper/Coordinators/AppCoordinator+RemoteNotifications.swift
@@ -0,0 +1,61 @@
+//
+//  AppCoordinator+RemoteNotifications.swift
+//  DropBit
+//
+//  Created by Ben Winters on 5/15/19.
+//  Copyright © 2019 Coin Ninja, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension AppCoordinator {
+
+  func registerForRemoteNotifications(with deviceToken: Data) {
+    let token = deviceToken.hexString
+    persistenceManager.set(token, for: .devicePushToken)
+
+    notificationManager.performRegistrationIfNeeded(forPushToken: token)
+  }
+
+  func requestPushNotificationDialogueIfNeeded() {
+    switch permissionManager.permissionStatus(for: .notification) {
+    case .authorized:
+      DispatchQueue.main.async {
+        UIApplication.shared.registerForRemoteNotifications()
+      }
+    case .disabled, .denied:
+      notificationManager.removeSubscriptions()
+    case .notDetermined:
+      showPushNotificationActionableAlert()
+    }
+  }
+
+  func showPushNotificationActionableAlert() {
+    let requestConfiguration = AlertActionConfiguration(title: "GOT IT", style: .default, action: { [weak self] in
+      self?.permissionManager.requestPermission(for: .notification) { status in
+        switch status {
+        case .authorized:
+          DispatchQueue.main.async {
+            UIApplication.shared.registerForRemoteNotifications()
+          }
+        default:
+          break
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+          self?.presentDropBitMeViewController(verifiedFirstTime: true)
+        }
+      }
+    })
+
+    let title = "Push notifications are an important part of the DropBit experience." +
+    " Without them you will not be notified to complete transactions which will cause them to expire."
+
+    let description = "Please allow us to send you push notifications on the following prompt."
+    let alert = alertManager.detailedAlert(withTitle: title, description: description, image: #imageLiteral(resourceName: "dropBitBadgeIcon"), style: .warning, action: requestConfiguration)
+
+    navigationController.topViewController()?.present(alert, animated: true)
+  }
+  
+}
--- CoinKeeper/Coordinators/AppCoordinator.swift
+++ CoinKeeper/Coordinators/AppCoordinator.swift
@@ -149,51 +149,8 @@ class AppCoordinator: CoordinatorType {
     self.serialQueueManager.delegate = self
   }
 
-  func registerForRemoteNotifications(with deviceToken: Data) {
-    let token = deviceToken.hexString
-    persistenceManager.set(token, for: .devicePushToken)
-
-    notificationManager.performRegistrationIfNeeded(forPushToken: token)
-  }
-
-  private func requestPushNotificationDialogueIfNeeded() {
-    switch permissionManager.permissionStatus(for: .notification) {
-    case .authorized:
-      DispatchQueue.main.async {
-        UIApplication.shared.registerForRemoteNotifications()
-      }
-    case .disabled, .denied:
-      notificationManager.removeSubscriptions()
-    case .notDetermined:
-      showPushNotificationActionableAlert()
-    }
-  }
-
-  private func showPushNotificationActionableAlert() {
-    let requestConfiguration = AlertActionConfiguration(title: "GOT IT", style: .default, action: { [weak self] in
-      self?.permissionManager.requestPermission(for: .notification) { status in
-        switch status {
-        case .authorized:
-          DispatchQueue.main.async {
-            UIApplication.shared.registerForRemoteNotifications()
-          }
-        default:
-          break
-        }
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-          self?.presentDropBitMeViewController(verifiedFirstTime: true)
-        }
-      }
-    })
-
-    let title = "Push notifications are an important part of the DropBit experience." +
-    " Without them you will not be notified to complete transactions which will cause them to expire."
-
-    let description = "Please allow us to send you push notifications on the following prompt."
-    let alert = alertManager.detailedAlert(withTitle: title, description: description, image: #imageLiteral(resourceName: "dropBitBadgeIcon"), style: .warning, action: requestConfiguration)
-
-    navigationController.topViewController()?.present(alert, animated: true)
+  var drawerController: MMDrawerController? {
+    return navigationController.topViewController.flatMap { $0 as? MMDrawerController }
   }
 
   private func setInitialRootViewController() {
@@ -337,10 +294,6 @@ class AppCoordinator: CoordinatorType {
       sendPaymentViewController.viewModel.updatePrimaryCurrency(to: currencyController.selectedCurrency)
       navigationController.present(sendPaymentViewController, animated: true)
     }
-  }
-
-  var drawerController: MMDrawerController? {
-    return navigationController.topViewController.flatMap { $0 as? MMDrawerController }
   }
 
   private func checkForBackendMessages() {
