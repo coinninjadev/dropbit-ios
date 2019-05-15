10da47f46fdfc201b59cb2e47ebc088f71242aae
Ben Winters <ben.winters@coinninja.com>
2019-05-15 12:59:40 -0400

    Move analytics functions to separate file

--- CoinKeeper/Coordinators/AppCoordinator.swift
+++ CoinKeeper/Coordinators/AppCoordinator.swift
@@ -379,59 +379,6 @@ class AppCoordinator: CoordinatorType {
     }
   }
 
-  private func trackAnalytics() {
-    trackEventForFirstTimeOpeningAppIfApplicable()
-    trackIfUserHasWallet()
-    trackIfUserHasWordsBackedUp()
-    trackIfDropBitMeIsEnabled()
-  }
-
-  private func trackEventForFirstTimeOpeningAppIfApplicable() {
-    if isFirstTimeOpeningApp {
-      analyticsManager.track(event: .firstOpen, with: nil)
-      persistenceManager.set(true, for: .firstTimeOpeningApp)
-    }
-  }
-
-  private func trackIfUserHasWallet() {
-    if walletManager == nil {
-      analyticsManager.track(property: MixpanelProperty(key: .hasWallet, value: false))
-    } else {
-      analyticsManager.track(property: MixpanelProperty(key: .hasWallet, value: true))
-    }
-  }
-
-  private func trackIfUserHasWordsBackedUp() {
-    if walletManager == nil || !wordsBackedUp {
-      analyticsManager.track(property: MixpanelProperty(key: .wordsBackedUp, value: false))
-    } else {
-      analyticsManager.track(property: MixpanelProperty(key: .wordsBackedUp, value: true))
-    }
-  }
-
-  private func trackIfDropBitMeIsEnabled() {
-    let bgContext = self.persistenceManager.createBackgroundContext()
-    bgContext.perform {
-      let isEnabled = self.persistenceManager.getUserPublicURLInfo(in: bgContext)?.isEnabled ?? false
-      self.analyticsManager.track(property: MixpanelProperty(key: .isDropBitMeEnabled, value: isEnabled))
-    }
-  }
-
-  func trackIfUserHasABalance() {
-    let bgContext = persistenceManager.createBackgroundContext()
-    guard let wmgr = walletManager else {
-      self.analyticsManager.track(property: MixpanelProperty(key: .hasBTCBalance, value: false))
-      return
-    }
-
-    var balanceIsPositive = false
-    bgContext.performAndWait {
-      balanceIsPositive = wmgr.spendableBalance(in: bgContext) > 0
-    }
-
-    analyticsManager.track(property: MixpanelProperty(key: .hasBTCBalance, value: balanceIsPositive ? true : false))
-  }
-
   private func applyUITestArguments(_ arguments: [UITestArgument]) {
     if arguments.isEmpty { return }
 
