b237cb31c9b0acaba5dcf79274eb080597a3d434
BJ Miller <bj.miller@coinninja.com>
2019-05-17 08:43:31 -0400

    Add static findOrCreate on CKMTwitterContact

--- CoinKeeper/Models/NSManagedObject/CKMTwitterContact+CoreDataClass.swift
+++ CoinKeeper/Models/NSManagedObject/CKMTwitterContact+CoreDataClass.swift
@@ -13,4 +13,30 @@ import CoreData
 @objc(CKMTwitterContact)
 public class CKMTwitterContact: NSManagedObject {
 
+  static func findOrCreate(with contact: TwitterContactType, in context: NSManagedObjectContext) -> CKMTwitterContact {
+    let fetchRequest: NSFetchRequest<CKMTwitterContact> = CKMTwitterContact.fetchRequest()
+    let idKeyPath = #keyPath(CKMTwitterContact.identityHash)
+    let predicate = NSPredicate(format: "%K == %@", idKeyPath, contact.identityHash)
+    fetchRequest.predicate = predicate
+    fetchRequest.fetchLimit = 1
+
+    var twitterContact: CKMTwitterContact!
+    do {
+      if let foundContact = try context.fetch(fetchRequest).first {
+        twitterContact = foundContact
+      } else {
+        twitterContact = CKMTwitterContact(insertInto: context)
+      }
+    } catch {
+      twitterContact = CKMTwitterContact(insertInto: context)
+    }
+
+    return twitterContact
+  }
+
+  private func configure(with contact: TwitterContactType, in context: NSManagedObjectContext) {
+    self.identityHash = contact.identityHash
+    self.displayName = contact.displayName ?? ""
+    self.displayScreenName = contact.displayIdentity
+  }
 }
