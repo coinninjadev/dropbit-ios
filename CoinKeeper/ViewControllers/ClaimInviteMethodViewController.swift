e978737704b2194533774da82fdf13c926943efa
Ben Winters <ben.winters@coinninja.com>
2019-05-21 17:29:48 -0400

    Add ClaimInviteMethodViewController

---
+++ CoinKeeper/ViewControllers/ClaimInviteMethodViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ClaimInviteMethodViewController.swift
+//  DropBit
+//
+//  Created by Ben Winters on 5/21/19.
+//  Copyright © 2019 Coin Ninja, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol ClaimInviteMethodViewControllerDelegate: AnyObject {
+  func viewControllerDidSelectClaimInvite(using method: UserIdentityType, viewController: UIViewController)
+}
+
+class ClaimInviteMethodViewController: BaseViewController, StoryboardInitializable {
+
+  private weak var delegate: ClaimInviteMethodViewControllerDelegate?
+
+  static func newInstance(delegate: ClaimInviteMethodViewControllerDelegate) -> ClaimInviteMethodViewController {
+    let vc = ClaimInviteMethodViewController.makeFromStoryboard()
+    vc.delegate = delegate
+    return vc
+  }
+
+  @IBOutlet var messageLabel: UILabel!
+  @IBOutlet var twitterButton: UIButton!
+  @IBOutlet var smsButton: UIButton!
+
+  @IBAction func performTwitter(_ sender: Any) {
+    delegate?.viewControllerDidSelectClaimInvite(using: .twitter, viewController: self)
+  }
+
+  @IBAction func performSMS(_ sender: Any) {
+    delegate?.viewControllerDidSelectClaimInvite(using: .phone, viewController: self)
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    messageLabel.text = "How were you sent Bitcoin?"
+    configureButtons()
+  }
+
+  private func configureButtons() {
+    twitterButton.backgroundColor = Theme.Color.primaryActionButton.color
+    smsButton.backgroundColor = Theme.Color.darkBlueButton.color
+    twitterButton.applyCornerRadius(5)
+    smsButton.applyCornerRadius(5)
+
+    let twitterTitle = NSAttributedString(imageName: "twitterBird",
+                                          imageSize: CGSize(width: 20, height: 17),
+                                          title: "I GOT A TWEET",
+                                          sharedColor: Theme.Color.lightGrayText.color,
+                                          font: Theme.Font.primaryButtonTitle.font)
+    twitterButton.setAttributedTitle(twitterTitle, for: .normal)
+
+    let smsTitle = NSAttributedString(imageName: "contactsIcon",
+                                      imageSize: CGSize(width: 13, height: 23),
+                                      title: "I GOT A SMS",
+                                      sharedColor: Theme.Color.lightGrayText.color,
+                                      font: Theme.Font.primaryButtonTitle.font)
+    smsButton.setAttributedTitle(smsTitle, for: .normal)
+  }
+
+}
