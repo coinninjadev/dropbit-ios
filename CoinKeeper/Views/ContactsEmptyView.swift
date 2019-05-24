7614885a30cb41101a8a37f22ff2344353fbed63
Ben Winters <ben.winters@coinninja.com>
2019-05-24 10:25:22 -0400

    Add ContactsEmptyView

---
+++ CoinKeeper/Views/ContactsEmptyView.swift
@@ -0,0 +1,44 @@
+//
+//  ContactsEmptyView.swift
+//  DropBit
+//
+//  Created by Ben Winters on 5/24/19.
+//  Copyright © 2019 Coin Ninja, LLC. All rights reserved.
+//
+
+import UIKit
+
+protocol ContactsEmptyViewDelegate: AnyObject {
+  func viewDidSelectPrimaryAction(_ view: UIView)
+}
+
+class ContactsEmptyView: UIView {
+
+  weak var delegate: ContactsEmptyViewDelegate?
+
+  @IBOutlet var descriptionLabel: UILabel!
+  @IBOutlet var primaryButton: PrimaryActionButton!
+  @IBAction func performPrimaryAction(_ sender: Any) {
+    delegate?.viewDidSelectPrimaryAction(self)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    xibSetup()
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    xibSetup()
+  }
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    backgroundColor = .clear
+    descriptionLabel.text = "Your contacts will be shown here once you allow DropBit to access them."
+    descriptionLabel.textColor = Theme.Color.grayText.color
+    descriptionLabel.font = Theme.Font.emptyStateDescription.font
+    primaryButton.setTitle("ALLOW ACCESS TO CONTACTS", for: .normal)
+  }
+
+}
