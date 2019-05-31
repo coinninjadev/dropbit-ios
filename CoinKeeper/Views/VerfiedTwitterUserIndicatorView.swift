c2d19df213470bbdcd00a765ca41e37ff381b938
BJ Miller <bj.miller@coinninja.com>
2019-05-31 15:48:47 -0400

    Add custom indicator view for verified twitter users

---
+++ CoinKeeper/Views/VerfiedTwitterUserIndicatorView.swift
@@ -0,0 +1,21 @@
+//
+//  VerfiedTwitterUserIndicatorView.swift
+//  DropBit
+//
+//  Created by BJ Miller on 5/31/19.
+//  Copyright © 2019 Coin Ninja, LLC. All rights reserved.
+//
+
+import UIKit
+
+class VerfiedTwitterUserIndicatorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
