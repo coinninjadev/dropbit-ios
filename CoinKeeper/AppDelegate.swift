f9d2522f0501df004963432c24ec26f1395b1f5e
BJ Miller <bj.miller@coinninja.com>
2019-06-06 14:34:27 -0400

    Wrap didFinishLaunching coordinator setup in nil check too

--- CoinKeeper/AppDelegate.swift
+++ CoinKeeper/AppDelegate.swift
@@ -17,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-    setupCoordinator()
+    if coordinator == nil {
+      setupCoordinator()
+    }
 
     return true
   }
