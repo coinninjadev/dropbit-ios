99395a6551ffbb1b0d4cacf85f36088ee2220373
Ben Winters <ben.winters@coinninja.com>
2019-07-03 13:29:43 -0400

    Remove unnecessary logs

--- CoinKeeper/Networking/NetworkManager/NetworkManager+AddressRequestable.swift
+++ CoinKeeper/Networking/NetworkManager/NetworkManager+AddressRequestable.swift
@@ -33,11 +33,10 @@ extension NetworkManager: AddressRequestable {
 
     let isIncremental = afterDate != nil
 
-    log.debug("fetching responses for addresses: page %d, addresses %@.", privateArgs: [page, addresses])
+    log.debug("fetching responses for addresses: page \(page).")
     return cnProvider.requestList(AddressesTarget.query(addresses, page, perPage, afterDate))
       .get { responses in
         if !isIncremental, responses.isEmpty, page == 1 {
-          log.debug("response is empty during full sync, rejecting with .emptyResponse")
           throw CKNetworkError.emptyResponse
         }
       }
@@ -45,11 +44,9 @@ extension NetworkManager: AddressRequestable {
         if responses.count == perPage {
           let nextPage = page + 1
           let combined = aggregateResponses + responses
-          log.debug("combined response count: \(combined.count)")
           return self.atsResponses(for: addresses, afterDate: afterDate, page: nextPage, perPage: perPage, with: combined)
 
         } else {
-          log.debug("responses count (\(responses.count)) less than perPage limit, returning aggregated responses")
           return Promise.value(aggregateResponses + responses)
         }
     }
