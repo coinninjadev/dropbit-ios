a7e4a50d98a4f1c8a7cff172c73f64a554be845f
Ben Winters <ben.winters@coinninja.com>
2019-07-18 09:54:40 -0400

    Add default implementations for CurrencySwappableAmountEditor

--- CoinKeeper/Protocols/CurrencySwappableAmountEditor.swift
+++ CoinKeeper/Protocols/CurrencySwappableAmountEditor.swift
@@ -8,17 +8,40 @@
 
 import Foundation
 
-protocol CurrencySwappableAmountEditor: CurrencySwappableEditAmountViewDelegate {
+protocol CurrencySwappableAmountEditor: CurrencySwappableEditAmountViewDelegate, CurrencySwappableEditAmountViewModelDelegate,
+  ExchangeRateUpdateable {
+
   var editAmountViewModel: CurrencySwappableEditAmountViewModel { get }
-  var editAmountView: CurrencySwappableEditAmountView { get }
+  var editAmountView: CurrencySwappableEditAmountView! { get }
 }
 
 extension CurrencySwappableAmountEditor {
 
+  /// Call this during viewDidLoad
+  func setupCurrencySwappableEditAmountView() {
+    editAmountView.primaryAmountTextField.delegate = editAmountViewModel
+
+    let textFieldDidChangeAction = #selector(CurrencySwappableEditAmountViewModel.primaryAmountTextFieldDidChange)
+    editAmountView.primaryAmountTextField.addTarget(editAmountViewModel,
+                                                    action: textFieldDidChangeAction,
+                                                    for: .editingChanged)
+  }
+
   func swapViewDidSwap(_ swapView: CurrencySwappableEditAmountView) {
     editAmountViewModel.swapPrimaryCurrency()
+    refreshAmounts()
+  }
+
+  /// Editor should call this in response to delegate method calls of CurrencySwappableEditAmountViewModelDelegate
+  func refreshAmounts() {
     let labels = editAmountViewModel.amountLabels(withSymbols: true)
-    editAmountView.update(labels)
+    editAmountView.update(with: labels)
+  }
+
+  func viewModelDidChangeAmount(_ viewModel: CurrencySwappableEditAmountViewModel) {
+    //Skip updating primary text field
+    let labels = viewModel.amountLabels(withSymbols: true)
+    editAmountView.secondaryAmountLabel.attributedText = labels.secondary
   }
 
 }
