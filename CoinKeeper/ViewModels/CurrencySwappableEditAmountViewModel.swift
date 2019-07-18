d1739b1c8d5bd8a48d037f8fb25cca9e3d406385
Ben Winters <ben.winters@coinninja.com>
2019-07-18 15:35:20 -0400

    Hide 0.00 in primary amount field

--- CoinKeeper/ViewModels/CurrencySwappableEditAmountViewModel.swift
+++ CoinKeeper/ViewModels/CurrencySwappableEditAmountViewModel.swift
@@ -30,6 +30,9 @@ extension DualAmountDisplayable {
     let primaryCurrency = currencyPair.primary
 
     var primaryLabel = currencyConverter.amountStringWithSymbol(forCurrency: primaryCurrency)
+    if currencyConverter.fromAmount == .zero {
+      primaryLabel = currencyConverter.fromCurrency.symbol
+    }
 
     let secondaryCurrency = currencyConverter.otherCurrency(forCurrency: primaryCurrency)
     var secondaryLabel = currencyConverter.attributedStringWithSymbol(forCurrency: secondaryCurrency)
