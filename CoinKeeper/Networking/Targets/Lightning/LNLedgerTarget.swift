47b90e04730fb83a3de2e75d3e0ff451cc800878
Ben Winters <ben.winters@coinninja.com>
2019-07-25 10:45:21 -0400

    Add LNLedgerTarget

---
+++ CoinKeeper/Networking/Targets/Lightning/LNLedgerTarget.swift
@@ -0,0 +1,48 @@
+//
+//  LNLedgerTarget.swift
+//  DropBit
+//
+//  Created by Ben Winters on 7/25/19.
+//  Copyright © 2019 Coin Ninja, LLC. All rights reserved.
+//
+
+import Moya
+
+struct LNLedgerResponse: ResponseDecodable {
+
+  let ledger: [LNTransactionResult]
+
+  static var sampleJSON: String {
+    return ""
+  }
+
+  static var requiredStringKeys: [KeyPath<LNLedgerResponse, String>] {
+    return []
+  }
+
+  static var optionalStringKeys: [WritableKeyPath<LNLedgerResponse, String?>] {
+    return []
+  }
+
+}
+
+public enum LNLedgerTarget: CoinNinjaTargetType {
+  typealias ResponseType = LNLedgerResponse
+
+  var basePath: String {
+    return "ledger"
+  }
+
+  var subPath: String? {
+    return nil
+  }
+
+  public var method: Method {
+    return .get
+  }
+
+  public var task: Task {
+    return .requestPlain
+  }
+
+}
