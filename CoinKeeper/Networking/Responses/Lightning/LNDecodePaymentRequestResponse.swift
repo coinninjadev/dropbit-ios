95947dd8b93e0f99fcd4887c74e96509217ad527
Ben Winters <ben.winters@coinninja.com>
2019-07-31 10:34:55 -0400

    Update response decoding and sample JSON for date format changes

--- CoinKeeper/Networking/Responses/Lightning/LNDecodePaymentRequestResponse.swift
+++ CoinKeeper/Networking/Responses/Lightning/LNDecodePaymentRequestResponse.swift
@@ -33,27 +33,6 @@ struct LNDecodePaymentRequestResponse: LNResponseDecodable {
   let cltvExpiry: Int
   let routeHints: [LNRouteHint]?
 
-  enum CodingKeys: String, CodingKey {
-    case destination, paymentHash, numSatoshis, timestamp, expiry,
-    description, descriptionHash, fallbackAddr, cltvExpiry, routeHints
-  }
-
-  init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-
-    destination = try container.decode(String.self, forKey: .destination)
-    paymentHash = try container.decode(String.self, forKey: .paymentHash)
-    numSatoshis = try container.decode(Int.self, forKey: .numSatoshis)
-    let timestampSeconds = try container.decode(Int.self, forKey: .timestamp)
-    self.timestamp = Date(timeIntervalSince1970: Double(timestampSeconds))
-    self.expiry = try container.decode(Int.self, forKey: .expiry)
-    self.description = try container.decode(String.self, forKey: .description).asNilIfEmpty()
-    self.descriptionHash = try container.decode(String.self, forKey: .descriptionHash).asNilIfEmpty()
-    self.fallbackAddr = try container.decode(String.self, forKey: .fallbackAddr).asNilIfEmpty()
-    self.cltvExpiry = try container.decode(Int.self, forKey: .cltvExpiry)
-    self.routeHints = try container.decodeIfPresent([LNRouteHint].self, forKey: .routeHints)
-  }
-
   static var sampleJSON: String {
     return """
     {
@@ -61,13 +40,11 @@ struct LNDecodePaymentRequestResponse: LNResponseDecodable {
     "destination" : "0357b3bb3fdedb369d2bdb429b6c397b207169bc8933e6f37e926e18b2f6c560f1",
     "expiry" : 3600,
     "fallback_addr" : "",
-    "route_hints" : [
-
-    ],
+    "route_hints" : nil,
     "description_hash" : "",
     "cltv_expiry" : 40,
-    "description" : "Test request generated at: 2019-07-29 15:03:29 +0000",
-    "timestamp" : 1564412609,
+    "description" : "Test request generated at: 2019-07-31 14:17:04 +0000",
+    "timestamp" : "2019-07-31T14:17:04.000000Z",
     "payment_hash" : "59da58f1b9ab6bb1b89659bfb5bd48a0221c40ddd2b5dcea3b25fb5843b58d9c"
     }
     """
--- CoinKeeper/Networking/Responses/Lightning/LNTransactionResponse.swift
+++ CoinKeeper/Networking/Responses/Lightning/LNTransactionResponse.swift
@@ -49,30 +49,6 @@ struct LNTransactionResult: LNResponseDecodable {
     return [] // Nested objects are not directly validated, refer to parent object
   }
 
-  enum CodingKeys: String, CodingKey {
-    case id, accountId, createdAt, updatedAt, expiresAt, status, type, direction, value, networkFee,
-    processingFee, request, memo, error
-  }
-
-  init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-
-    id = try container.decode(String.self, forKey: .id)
-    accountId = try container.decodeIfPresent(String.self, forKey: .accountId)
-    createdAt = try container.decode(Date.self, forKey: .createdAt)
-    updatedAt = try container.decodeIfPresent(Date.self, forKey: .updatedAt)
-    expiresAt = try container.decodeIfPresent(Date.self, forKey: .expiresAt)
-    status = try container.decode(LNTransactionStatus.self, forKey: .status)
-    type = try container.decode(LNTransactionType.self, forKey: .type)
-    direction = try container.decode(LNTransactionDirection.self, forKey: .direction)
-    value = try container.decode(Int.self, forKey: .value)
-    networkFee = try container.decode(Int.self, forKey: .value)
-    processingFee = try container.decode(Int.self, forKey: .value)
-    request = try container.decode(String.self, forKey: .request).asNilIfEmpty()
-    memo = try container.decode(String.self, forKey: .memo).asNilIfEmpty()
-    error = try container.decode(String.self, forKey: .error).asNilIfEmpty()
-  }
-
 }
 
 struct LNTransactionResponse: LNResponseDecodable {
