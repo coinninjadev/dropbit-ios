0b1ecb376920fb3cf5b76a26ca02524b7dfa891c
Ben Winters <ben.winters@coinninja.com>
2019-07-31 16:18:57 -0400

    Add TransactionHistoryDetailCellDisplayable

---
+++ CoinKeeper/ViewModels/TransactionHistoryDetailCellDisplayable.swift
@@ -0,0 +1,120 @@
+//
+//  TransactionHistoryDetailCellDisplayable.swift
+//  DropBit
+//
+//  Created by Ben Winters on 7/31/19.
+//  Copyright © 2019 Coin Ninja, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Provides all variable values directly necessary to configure the TransactionHistoryDetailCell UI.
+/// Fixed values (colors, font sizes, etc.) are contained by the cell itself.
+protocol TransactionHistoryDetailCellDisplayable {
+  var direction: TransactionDirection { get }
+  var status: TransactionStatus { get }
+  var statusText: String { get }
+  var statusTextColor: UIColor { get }
+  var progressConfig: ProgressBarConfig? { get }
+  var twitterConfig: DetailCellTwitterConfig? { get }
+  var recipientDescription: String? { get }
+  var bitcoinAddress: String? { get }
+  var amountLabels: DetailCellAmountLabels { get }
+  var memoConfig: DetailCellMemoConfig? { get }
+  var displayDate: String { get }
+  var actionButtonConfig: DetailCellActionButtonConfig? { get }
+}
+
+extension TransactionHistoryDetailCellDisplayable {
+  var directionImage: UIImage? {
+    switch direction {
+    case .in:   return UIImage(named: "incomingDetailIcon")
+    case .out:  return UIImage(named: "outgoingDetailIcon")
+    }
+  }
+
+  var statusTextColor: UIColor {
+    return .lightGrayText
+  }
+}
+
+protocol TransactionHistoryInvalidDetailCellDisplayable: TransactionHistoryDetailCellDisplayable { }
+
+extension TransactionHistoryInvalidDetailCellDisplayable {
+
+  var statusTextColor: UIColor {
+    return .warning
+  }
+
+  var directionImage: UIImage? {
+    return UIImage(named: "invalidDetailIcon")
+  }
+
+  var warningMessage: String? {
+    switch status {
+    case .expired:
+      return """
+      For security reasons we can only allow
+      48 hours to accept a transaction.
+      This transaction has expired.
+      """
+    default:
+      return nil
+    }
+  }
+}
+
+struct ProgressBarConfig {
+  let titles: [String]
+  let stepTitles: [String]
+  let width: CGFloat
+  let selectedTabIndex: Int
+}
+
+struct LightningInvoiceDisplayDetails {
+  let invoiceStatus: InvoiceStatus
+  let qrCode: UIImage
+  let request: String
+  let memo: String?
+
+  enum InvoiceStatus {
+    case pending(Int) //associated value is hours remaining
+    case expired
+    case paid
+  }
+
+  var canRemoveFromTransactionList: Bool {
+    if case .expired = invoiceStatus {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+struct DetailCellActionButtonConfig {
+  let title: String
+  let backgroundColor: UIColor
+}
+
+struct DetailCellMemoConfig {
+  let memo: String
+  let isShared: Bool
+  let sharingDescription: String?
+  let sharingIcon: UIImage?
+}
+
+struct DetailCellTwitterConfig {
+  let avatar: UIImage
+  let accessory: UIImage
+  let displayHandle: String
+}
+
+/// Only one of the secondary strings should be set
+struct DetailCellAmountLabels {
+  let primaryText: String
+  let secondaryText: String?
+  let secondaryAttributedText: NSAttributedString?
+  let historicalPriceAttributedText: NSAttributedString?
+}
--- CoinKeeper/ViewModels/TransactionHistoryDetailCellViewModelType.swift
+++ CoinKeeper/ViewModels/TransactionHistoryDetailCellViewModelType.swift
@@ -9,6 +9,48 @@
 import Foundation
 import UIKit
 
+/// Defines inputs necessary to determine the values displayed in the cell.
+protocol TransactionHistoryDetailCellViewModelType: TransactionHistoryDetailCellDisplayable {
+  var isValidTransaction: Bool { get }
+  var accountType: AccountType { get }
+  var isLightningTransfer: Bool { get }
+  var amountDetails: TransactionAmountDetails { get }
+  var action: TransactionDetailAction? { get }
+}
+
+extension TransactionHistoryDetailCellViewModelType {
+
+  var isValidTransaction: Bool {
+    return true
+  }
+
+  var isLightningTransfer: Bool {
+    return false
+  }
+
+  var statusTextColor: UIColor {
+    return .lightGrayText
+  }
+
+  var shouldShowStatusPill: Bool {
+    return false
+  }
+
+  var canAddMemo: Bool {
+    if isLightningTransfer { return false }
+    return memoConfig == nil
+  }
+
+  var bitcoinAddressURL: URL? {
+    return nil
+  }
+
+  var warningMessage: String? {
+    return nil
+  }
+
+}
+
 enum TransactionDirection: String {
   case `in`, out
 }
@@ -17,75 +59,18 @@ enum AccountType {
   case bitcoin, lightning
 }
 
-struct ProgressBarViewModel {
-  let titles: [String]
-  let stepTitles: [String]
-  let width: CGFloat
-  let selectedTabIndex: Int
+enum TransactionStatus {
+  case pending
+  case broadcasting
+  case completed
+  case canceled
+  case expired
 }
 
-struct TwitterDisplayDetails {
-  let handle: String
-  let avatar: UIImage
-  let twitterIcon: UIImage
-}
-
-struct LightningDisplayDetails {
-  let qrCode: UIImage
-  let request: String
-  let memo: String?
-}
-
-struct AmountDisplayDetails {
+struct TransactionAmountDetails {
   let currencyPair: CurrencyPair
   let exchangeRates: ExchangeRates
   let primaryBTCAmount: NSDecimalNumber
   let fiatWhenCreated: NSDecimalNumber?
   let fiatWhenTransacted: NSDecimalNumber?
-}
-
-struct MemoDisplayDetails {
-  let memo: String
-  let isShared: Bool
-  let sharingDescription: String?
-  let sharingIcon: UIImage?
-}
-
-enum TransactionDetailsAction {
-  case detailsURL(URL)
-  case detailsPopover
-  case cancel
-  case remove
-}
-
-/// This interface is designed with the intention that if something is nil,
-/// the corresponding view(s) should be hidden.
-protocol TransactionHistoryDetailCellViewModelType {
-
-  var isValidTransaction: Bool { get }
-  var accountType: AccountType { get }
-
-  var direction: TransactionDirection { get }
-  var isLightningTransfer: Bool { get }
-  var statusDescription: String { get }
-  var statusTextColor: UIColor { get }
-  var shouldShowStatusPill: Bool { get }
-  var progressBarViewModel: ProgressBarViewModel? { get }
-
-  var recipientDescription: String? { get }
-
-  var bitcoinAddress: String? { get }
-
-  var twitterDetails: TwitterDisplayDetails? { get }
-  var lightningDetails: LightningDisplayDetails? { get }
-
-  var amountDetails: AmountDisplayDetails { get }
-
-  var canAddMemo: Bool { get }
-  var memoDetails: MemoDisplayDetails? { get }
-
-  var bitcoinAddressURL: URL? { get }
-  var action: TransactionDetailsAction? { get }
-  var warningMessage: String? { get }
-
 }
