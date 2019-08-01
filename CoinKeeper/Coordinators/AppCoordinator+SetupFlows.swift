a765b9bdd4668d7a792bcefce3e801e5f546278a
BJ Miller <bj.miller@coinninja.com>
2019-08-01 14:15:56 -0400

    Call different restore method after icloud restore, update test to reflect new logic

--- CoinKeeper/Coordinators/AppCoordinator+SetupFlows.swift
+++ CoinKeeper/Coordinators/AppCoordinator+SetupFlows.swift
@@ -86,7 +86,7 @@ extension AppCoordinator {
   func startFirstTimeAfteriCloudRestore() {
     let title = ""
     let description = "It looks like you have restored from a backup. Please enter your 12 recovery words to restore your wallet."
-    let okAction = AlertActionConfiguration(title: "RESTORE NOW", style: .default) { self.restoreWallet() }
+    let okAction = AlertActionConfiguration(title: "RESTORE NOW", style: .default) { self.restoreWalletAfterICloudRestore() }
     let alertViewModel = AlertControllerViewModel(title: title, description: description, actions: [okAction])
     let alert = alertManager.alert(from: alertViewModel)
     navigationController.topViewController()?.present(alert, animated: true)
