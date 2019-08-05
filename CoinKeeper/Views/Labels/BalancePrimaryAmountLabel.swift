46e36b1457c7d7b81b5f435b70df765083f0566c
Mitch Malleo <mitch.malleo@coinninja.com>
2019-08-05 15:38:35 -0400

    Fix bitcoin image clipping when primary currency

--- CoinKeeper/Views/Labels/BalancePrimaryAmountLabel.swift
+++ CoinKeeper/Views/Labels/BalancePrimaryAmountLabel.swift
@@ -17,4 +17,9 @@ class BalancePrimaryAmountLabel: UILabel {
     numberOfLines = 1
     textAlignment = .right
   }
+
+  override func drawText(in rect: CGRect) {
+    let insets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+    super.drawText(in: rect.inset(by: insets))
+  }
 }
