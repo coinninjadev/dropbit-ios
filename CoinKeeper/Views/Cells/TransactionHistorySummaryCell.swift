cb78a05ce94e9228e8bbe62f772da2891f7c1e82
Ben Winters <ben.winters@coinninja.com>
2019-08-22 16:14:48 -0400

    Delete func for loading old view model

--- CoinKeeper/Views/Cells/TransactionHistorySummaryCell.swift
+++ CoinKeeper/Views/Cells/TransactionHistorySummaryCell.swift
@@ -71,45 +71,4 @@ class TransactionHistorySummaryCell: UICollectionViewCell {
     memoLabel.text = values.memo
   }
 
-  /*
-  func load(with viewModel: OldTransactionSummaryCellViewModel, isAtTop: Bool = false) {
-    if viewModel.isTwitterContact, let avatar = viewModel.counterpartyAvatar {
-      incomingImage.image = avatar
-      let radius = incomingImage.frame.width / 2.0
-      incomingImage.applyCornerRadius(radius)
-      let borderColor: UIColor = viewModel.isIncoming ? .appleGreen : .darkPeach
-      incomingImage.layer.borderColor = borderColor.cgColor
-      incomingImage.layer.borderWidth = 2.0
-    } else {
-      if viewModel.transactionIsInvalidated {
-        incomingImage.image = UIImage(named: "invalidated30")
-      } else {
-        incomingImage.image = viewModel.isIncoming ? UIImage(named: "incoming30") : UIImage(named: "outgoing30")
-      }
-    }
-
-    receiverLabel.text = viewModel.counterpartyDescription.isEmpty ? viewModel.receiverAddress : viewModel.counterpartyDescription
-    twitterImage.isHidden = !viewModel.isTwitterContact
-    statusLabel.text = viewModel.statusDescription
-    statusLabel.isHidden = viewModel.hidden
-
-    statusLabel.textColor = viewModel.descriptionColor
-
-    dateLabel.text = viewModel.dateDescriptionFull
-
-    // fall back to sent amount if still pending and fees are not known
-    let converter = viewModel.receivedAmountAtCurrentConverter ?? viewModel.sentAmountAtCurrentConverter
-    let labels = viewModel.amountLabels(for: converter)
-
-    primaryAmountLabel.text = labels.primary
-    secondaryAmountLabel.text = labels.secondary
-    primaryAmountLabel.textColor = viewModel.isIncoming ? .darkBlueText : .darkPeach
-
-    memoLabel.text = viewModel.memo
-    memoLabel.isHidden = viewModel.memo.isEmpty
-
-    layer.maskedCorners = isAtTop ? [.layerMinXMinYCorner, .layerMaxXMinYCorner] : []
-  }
-  */
-
 }
