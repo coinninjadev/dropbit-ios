19bddee9f2cf17aed1d755af15c29142959ecaf9
Ben Winters <ben.winters@coinninja.com>
2019-08-22 08:41:21 -0400

    testIncomingOnChain_LightningTransfer_loadsImageAndColor

--- CoinKeeperTests/Cells/TransactionHistorySummaryCellTests.swift
+++ CoinKeeperTests/Cells/TransactionHistorySummaryCellTests.swift
@@ -99,6 +99,14 @@ class TransactionHistorySummaryCellTests: XCTestCase {
     XCTAssertEqual(sut.leadingDirectionBackgroundView.backgroundColor, expectedColor)
   }
 
+  func testIncomingOnChain_LightningTransfer_loadsImageAndColor() {
+    let viewModel = createTestViewModel(walletTxType: .onChain, direction: .in, isLightningTransfer: true)
+    sut.configure(with: viewModel)
+    let expectedImage = viewModel.transferImage, expectedColor = UIColor.incomingGreen
+    XCTAssertEqual(sut.leadingDirectionImage.image, expectedImage)
+    XCTAssertEqual(sut.leadingDirectionBackgroundView.backgroundColor, expectedColor)
+  }
+
   // MARK: load method
 //  func testLoadMethodPopoulatesOutlets() {
 //    // given
