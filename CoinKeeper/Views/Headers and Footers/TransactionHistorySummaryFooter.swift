8e553f33c96e66e07a6cb30e34d5a71eddeb09ff
Ben Winters <ben.winters@coinninja.com>
2019-09-04 12:12:43 -0400

    Add summary footer files

---
+++ CoinKeeper/Views/Headers and Footers/TransactionHistorySummaryFooter.swift
@@ -0,0 +1,13 @@
+//
+//  TransactionHistorySummaryFooter.swift
+//  DropBit
+//
+//  Created by Ben Winters on 9/4/19.
+//  Copyright © 2019 Coin Ninja, LLC. All rights reserved.
+//
+
+import UIKit
+
+class TransactionHistorySummaryFooter: UICollectionReusableView {
+
+}
