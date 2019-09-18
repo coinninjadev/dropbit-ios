f3924b366aba7576ec9b31f663b4a7ebdee7e93d
Ben Winters <ben.winters@coinninja.com>
2019-09-18 17:14:09 -0400

    Add semi-transparent white circle behind close button on news screen

--- CoinKeeper/Extensions/UIColor+Init.swift
+++ CoinKeeper/Extensions/UIColor+Init.swift
@@ -14,8 +14,8 @@ extension UIColor {
     self.init(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: a)
   }
 
-  convenience init(gray value: CGFloat) {
-    self.init(r: value, g: value, b: value)
+  convenience init(gray value: CGFloat, alpha: CGFloat = 1.0) {
+    self.init(r: value, g: value, b: value, a: alpha)
   }
 
 }
