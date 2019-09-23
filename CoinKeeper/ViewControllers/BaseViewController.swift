b33c74ee0650bee16e69759f1ad740273b489c68
Mitch Malleo <mitch.malleo@coinninja.com>
2019-09-23 13:48:23 -0400

    Add early return for lock/unlock notifications

--- CoinKeeper/ViewControllers/BaseViewController.swift
+++ CoinKeeper/ViewControllers/BaseViewController.swift
@@ -52,11 +52,25 @@ extension BaseViewController {
 
   fileprivate func registerForLockStatusNotification() {
     lockStatusNotification = CKNotificationCenter.subscribe(key: .didLockLightning, object: nil, queue: .main, using: { [weak self] _ in
+      switch BaseViewController.lockStatus {
+      case .locked:
+        return
+      default:
+        break
+      }
+
       BaseViewController.lockStatus = .locked
       self?.lock()
     })
 
     unlockStatusNotification = CKNotificationCenter.subscribe(key: .didUnlockLightning, object: nil, queue: .main, using: { [weak self] _ in
+      switch BaseViewController.lockStatus {
+      case .unlocked:
+        return
+      default:
+        break
+      }
+
       BaseViewController.lockStatus = .unlocked
       self?.unlock()
     })
