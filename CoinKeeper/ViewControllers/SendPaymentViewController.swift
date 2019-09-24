d43aadc9f900813398ecea9f0f0faa130391bcca
Mitch Malleo <mitch.malleo@coinninja.com>
2019-09-24 15:28:25 -0400

    Re-enable amount text field if user changes the recipient of a payment

--- CoinKeeper/ViewControllers/SendPaymentViewController.swift
+++ CoinKeeper/ViewControllers/SendPaymentViewController.swift
@@ -140,6 +140,7 @@ CurrencySwappableAmountEditor {
   @IBAction func performStartPhoneEntry() {
     showPhoneEntryView(with: "")
     phoneNumberEntryView.textField?.becomeFirstResponder()
+    editAmountView.isUserInteractionEnabled = true
   }
 
   /// Each button should connect to this IBAction. This prevents automatically
@@ -552,9 +553,11 @@ extension SendPaymentViewController {
 }
 
 extension SendPaymentViewController: SelectedValidContactDelegate {
+
   func update(withSelectedContact contact: ContactType) {
     setPaymentRecipient(.contact(contact))
     updateViewWithModel()
+    editAmountView.isUserInteractionEnabled = true
   }
 
   func update(withSelectedTwitterUser twitterUser: TwitterUser) {
