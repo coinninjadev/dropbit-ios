d7c04e978eeb18327a37fca5a5bf9766239ef1c8
Ben Winters <ben.winters@coinninja.com>
2019-10-04 17:51:51 -0400

    Provide AddressViewConfig with walletTxType

--- CoinKeeper/ViewModels/Transaction Cells/TransactionDetailCellProtocols.swift
+++ CoinKeeper/ViewModels/Transaction Cells/TransactionDetailCellProtocols.swift
@@ -246,7 +246,8 @@ extension TransactionDetailCellViewModelType {
 
   /// This struct provides a subset of the values so that the address view doesn't hold a reference to the full object
   var addressViewConfig: AddressViewConfig {
-    return AddressViewConfig(receiverAddress: receiverAddress,
+    return AddressViewConfig(walletTxType: walletTxType,
+                             receiverAddress: receiverAddress,
                              addressProvidedToSender: addressProvidedToSender,
                              broadcastFailed: (status == .failed && walletTxType == .onChain),
                              invitationStatus: invitationStatus)
