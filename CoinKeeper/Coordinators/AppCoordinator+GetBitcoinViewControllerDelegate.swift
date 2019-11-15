e9782c1d9a2f34e4ffc59f41f53770451df8baa7
Mitch Malleo <mitch.malleo@coinninja.com>
2019-11-15 13:03:22 +0000

    Feature/buy redesign

--- CoinKeeper/Coordinators/AppCoordinator+GetBitcoinViewControllerDelegate.swift
+++ CoinKeeper/Coordinators/AppCoordinator+GetBitcoinViewControllerDelegate.swift
@@ -17,12 +17,13 @@ extension AppCoordinator: GetBitcoinViewControllerDelegate {
     permissionManager.requestPermission(for: .location) { (status) in
       switch status {
       case .authorized, .notDetermined:
-        guard let coordinate = self.locationManager.location?.coordinate,
-          let url = CoinNinjaUrlFactory.buildUrl(for: .buyAtATM(coordinate)) else {
-            self.locationManager.requestLocation() //Attempt to repair for next location request
-            return
+        if self.locationManager.location?.coordinate == nil {
+          self.locationManager.requestLocation() //Attempt to repair for next location request
         }
-        self.openURL(url, completionHandler: nil)
+
+        if let url = CoinNinjaUrlFactory.buildUrl(for: .buyAtATM(self.locationManager.location?.coordinate)) {
+          self.openURL(url, completionHandler: nil)
+        }
       case .denied, .disabled:
         let description = "To use the location-based services of finding Bitcoin ATMs near you," +
         " please enable location services in the iOS Settings app."
@@ -32,15 +33,16 @@ extension AppCoordinator: GetBitcoinViewControllerDelegate {
     }
   }
 
-  func viewControllerBuyWithApplePay(_ viewController: GetBitcoinViewController, address: String) {
+  func viewControllerBuyWithApplePay(_ viewController: GetBitcoinViewController, bitcoinAddress: String) {
     analyticsManager.track(event: .buyWithQuickPay, with: nil)
-    guard address.asNilIfEmpty() != nil else {
+    guard bitcoinAddress.asNilIfEmpty() != nil else {
       let warning = "An error occurred: No Bitcoin receive address was detected. Please try again later."
       self.alertManager.showError(message: warning, forDuration: 2.0)
       return
     }
     viewControllerRequestedAuthenticationSuspension(viewController)
-    guard let url = CoinNinjaUrlFactory.buildUrl(for: .buyWithApplePay(address)) else { return }
+    //TODO: How do we want to handle all apple pay requests with a bitcoin address?
+    guard let url = CoinNinjaUrlFactory.buildUrl(for: .buyWithApplePay(bitcoinAddress)) else { return }
     openURLExternally(url, completionHandler: nil)
   }
 
---
+++ CoinKeeper/Views/Subviews/BuyMerchantAttributeView.swift
@@ -0,0 +1,72 @@
+//
+//  BuyMerchantAttributeView.swift
+//  DropBit
+//
+//  Created by Mitchell Malleo on 11/12/19.
+//  Copyright © 2019 Coin Ninja, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol BuyMerchantAttributeViewDelegate: AnyObject {
+  func attributeViewWasTouched(with url: URL)
+}
+
+class BuyMerchantAttributeView: UIView {
+
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var descriptionLabel: UILabel!
+  @IBOutlet var linkButton: UIButton!
+
+  weak var delegate: BuyMerchantAttributeViewDelegate?
+  var viewModel: BuyMerchantAttribute?
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    initialize()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    initialize()
+  }
+
+  private func initialize() {
+    xibSetup()
+  }
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+
+    backgroundColor = .lightGrayBackground
+    linkButton.contentHorizontalAlignment = .left
+  }
+
+  func load(with model: BuyMerchantAttribute) {
+    imageView.image = model.image
+    viewModel = model
+
+    if model.link != nil {
+      let normalString = NSMutableAttributedString.regular(model.description,
+                                                          size: 14, color: .darkBlueText)
+      normalString.underlineText()
+      linkButton.setAttributedTitle(normalString, for: .normal)
+
+      linkButton.isHidden = false
+      descriptionLabel.isHidden = true
+    } else {
+      descriptionLabel.attributedText = NSMutableAttributedString.regular(model.description,
+                                                                         size: 14, color: .darkBlueText)
+
+      linkButton.isHidden = true
+      descriptionLabel.isHidden = false
+    }
+
+  }
+
+  @IBAction func linkButtonWasTapped() {
+    guard let link = viewModel?.link, let url = URL(string: link) else { return }
+    delegate?.attributeViewWasTouched(with: url)
+  }
+}
