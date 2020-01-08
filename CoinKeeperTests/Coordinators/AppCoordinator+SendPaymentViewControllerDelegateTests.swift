f238bb527868418900501a683ac8741a8c991fcd
BJ Miller <bj.miller@coinninja.com>
2020-01-08 15:06:09 -0500

    Fix dependencies for promise-based invoice tests

--- CoinKeeperTests/Coordinators/AppCoordinator+SendPaymentViewControllerDelegateTests.swift
+++ CoinKeeperTests/Coordinators/AppCoordinator+SendPaymentViewControllerDelegateTests.swift
@@ -13,13 +13,20 @@ import XCTest
 class AppCoordinatorSendPaymentViewControllerDelegateTests: XCTestCase {
 
   var sut: AppCoordinator!
+  var walletManager: WalletManagerType?
 
   override func setUp() {
     super.setUp()
+    let mockPersistenceManager = MockPersistenceManager()
+    mockPersistenceManager.fakeCoinToUse = BTCMainnetCoin(purpose: .segwit)
+    let words = TestHelpers.abandonAbandon()
+    walletManager = WalletManager(words: words, persistenceManager: mockPersistenceManager)!
     sut = AppCoordinator()
+    sut.walletManager = walletManager
   }
 
   override func tearDown() {
+    walletManager = nil
     sut = nil
     super.tearDown()
   }
@@ -30,35 +37,33 @@ class AppCoordinatorSendPaymentViewControllerDelegateTests: XCTestCase {
     let url = LightningURL(string: invoiceString)!
     let expectedNumSatoshis = 250000
     let expectedDescription = "1 cup coffee"
-    let exp = XCTestExpectation(description: "ln decode invoice")
+    let exp = expectation(description: "ln decode valid invoice")
 
     sut.viewControllerDidReceiveLightningURLToDecode(url)
       .done { (response: LNDecodePaymentRequestResponse) in
         XCTAssertEqual(response.numSatoshis ?? 0, expectedNumSatoshis)
         XCTAssertEqual(response.description ?? "", expectedDescription)
-        exp.fulfill()
-    }
-    .catch { (error: Error) in
-      XCTFail(error.localizedDescription)
-    }
-    wait(for: [exp], timeout: 3.0)
+      }
+      .catch { (error: Error) in XCTFail(error.localizedDescription) }
+      .finally { exp.fulfill() }
+
+    waitForExpectations(timeout: 3.0)
   }
 
   func testLNDecodingWithInvalidInvoice() {
     // swiftlint:disable line_length
     let invoiceString = "lightning:lnbc2500u1pvjluezpp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdq5xysxxatsyp3k7enxv4jsxqzpuaztrnwngzn3kdzw5hydlzf03qdgm2hdq27cqv3agm2awhz5se903vruatfhq77w3ls4evs3ch9zw97j25emudupq63nyw24cg27h2rsp"
     let url = LightningURL(string: invoiceString)!
-    let exp = XCTestExpectation(description: "ln decode invoice")
+    let exp = expectation(description: "ln decode invalid invoice")
 
     sut.viewControllerDidReceiveLightningURLToDecode(url)
-      .done { (_) in
-        XCTFail("parsing should fail with checksum failed message")
-    }
-    .catch { (error: Error) in
-      let expectedMessage = "checksum failed"
-      XCTAssertTrue(error.localizedDescription.contains(expectedMessage))
-      exp.fulfill()
-    }
-    wait(for: [exp], timeout: 3.0)
+      .done { (_) in XCTFail("parsing should fail with checksum failed message") }
+      .catch { (error: Error) in
+        let expectedMessage = "checksum failed"
+        XCTAssertTrue(error.localizedDescription.contains(expectedMessage))
+      }
+      .finally { exp.fulfill() }
+
+    waitForExpectations(timeout: 3.0)
   }
 }
