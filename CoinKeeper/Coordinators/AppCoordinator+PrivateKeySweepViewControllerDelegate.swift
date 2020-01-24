b44d8257dfd3bc81e70380fa287b53e6581acad0
BJ Miller <bj.miller@coinninja.com>
2020-01-24 08:12:48 -0500

    Add mixpanel event when sweeping private key

--- CoinKeeper/Coordinators/AppCoordinator+PrivateKeySweepViewControllerDelegate.swift
+++ CoinKeeper/Coordinators/AppCoordinator+PrivateKeySweepViewControllerDelegate.swift
@@ -28,6 +28,7 @@ extension AppCoordinator: PrivateKeySweepViewControllerDelegate {
                                forViewController viewController: SuccessFailViewController) {
     networkManager.broadcastTx(with: transactionData)
       .done { (txid: String) in
+        self.analyticsManager.track(event: .sweepPrivateKey, with: nil)
         let context = self.persistenceManager.createBackgroundContext()
         let outgoingTransactionData = OutgoingTransactionData(txid: txid, destinationAddress: transactionData.paymentAddress,
                                                               amount: transactionData.amount, feeAmount: transactionData.feeAmount,
