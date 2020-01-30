5f84d5b5bcaeb082cc2dda50e2f91fae7cd94731
BJ Miller <bj.miller@coinninja.com>
2020-01-30 11:51:47 -0500

    Add missing :// to dropbit callback url for twitter

--- CoinKeeper/Networking/Twitter/NetworkManager+Twitter.swift
+++ CoinKeeper/Networking/Twitter/NetworkManager+Twitter.swift
@@ -35,7 +35,7 @@ protocol TwitterRequestable: AnyObject {
 
 extension TwitterOAuth {
   var callbackURL: String {
-    return DropBitUrlFactory.DropBitURL.scheme
+    return DropBitUrlFactory.DropBitURL.scheme + "://"
   }
 }
 
